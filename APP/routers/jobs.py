4af4c267e8ea88d70c3e8c8d94c36c8c2ad448c3
Nikol Bratkova <nikolbratkova@gmail.com>
2022-10-16 10:43:06 +0300

    update job + pagination

--- APP/routers/jobs.py
+++ APP/routers/jobs.py
@@ -18,8 +18,18 @@ def create(job_descr: JobAd, token = Header()):
 
 
 @jobs_router.get('/', tags=[Tags.jobs])
-def view_jobs(city_id: Cities | None = None, sector_id: JobSectors | None = None, type_id: EmplType| None = None, search: str| None = None):
+def view_jobs(city_id: Cities | None = None, sector_id: JobSectors | None = None, type_id: EmplType| None = None, search: str| None = None, offset: int = 0, limit: int = 10):
 
-    job_ads = job_service.view_jobs(city_id, sector_id, type_id, search)
+    job_ads = job_service.view_jobs(city_id, sector_id, type_id, search, offset, limit)
 
     return job_ads
+
+
+@jobs_router.put('/{id}', response_model=JobAdRepr, tags=[Tags.jobs])
+def update(id: int, job_descr: JobAd, token = Header()):
+    user = get_user_or_raise_401(token)
+    if user_service.get_user_type(user.id) != 'employer':
+        return BadRequest('User is not an employer')
+
+    job_ad = job_service.update_job(job_id=id, job_descr=job_descr)
+    return job_ad or BadRequest(content='Database constrains are violated')
